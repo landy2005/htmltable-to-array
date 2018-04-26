1e1e80f635db53925f057b47d84095303803352a
Mike Stupalov <mike@stupalov.com>
2018-04-26 14:42:55 +0300

    Added basic unittests.

---
+++ tests/HTMLTable2ArrayTest.php
@@ -0,0 +1,61 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class HTMLTable2ArrayTest extends TestCase
+{
+    private $DataDir = __DIR__ . '/Data/';
+
+    /**
+    * @dataProvider providerTable2Array
+    * @group local
+    */
+    public function testTable2Array($file, $json, $params)
+    {
+        $file_html = $this->DataDir . $file . '.html';
+        $file_json = $this->DataDir . $json . '.json';
+        if (!is_file($file_html)) {
+            $this->markTestSkipped('Testing files not found.');
+        }
+
+        $helper = new HTMLTable2Array($params);
+        $html = file_get_contents($file_html);
+        //var_dump($html);
+        $result = $helper->tableToArray(NULL, NULL, $html);
+
+        if (!is_file($file_json)) {
+            var_dump($result);
+            file_put_contents($file_json, json_encode($result));
+            $this->markTestSkipped('Just JSON file created.');
+        }
+        $test_content = file_get_contents($file_json);
+
+        switch ($params['format']) {
+            case 'json':
+                $test = $test_content;
+                break;
+            default:
+                $test = json_decode($test_content, TRUE);
+        }
+
+        $this->assertSame($result, $test);
+    }
+
+    public function providerTable2Array()
+    {
+        $array = [];
+        $params = [];
+        $params_json = ['format' => 'json'];
+
+        $array[] = ['simple-table-error', 'simple-table-error',         $params];
+        $array[] = ['multiple-tables',    'multiple-tables',            $params];
+        $array[] = ['simple-table-error', 'simple-table-error',         $params_json];
+        $array[] = ['multiple-tables',    'multiple-tables',            $params_json];
+        $array[] = ['multiple-tables',    'multiple-tables-tableAll',   ['tableAll' => TRUE]];
+        $array[] = ['multiple-tables',    'multiple-tables-tableAll',   ['tableAll' => TRUE, 'format' => 'json']];
+        $array[] = ['multiple-tables',    'multiple-tables-tableID',    ['tableID' => 'test1']];
+
+        return $array;
+    }
+
+}
