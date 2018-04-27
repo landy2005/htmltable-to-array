bfc808caec51a4ae45318695c2aea841cf23527b
Mike Stupalov <mike@stupalov.com>
2018-04-27 11:49:18 +0300

    Added unittest for options ignoreColumns and onlyColumns.

--- tests/HTMLTable2ArrayTest.php
+++ tests/HTMLTable2ArrayTest.php
@@ -55,11 +55,16 @@ final class HTMLTable2ArrayTest extends TestCase
         $array[] = ['multiple-tables',    'multiple-tables-tableAll',   ['tableAll' => TRUE, 'format' => 'json']];
         $array[] = ['multiple-tables',    'multiple-tables-tableID',    ['tableID' => 'test1']];
         $array[] = ['multiple-tables',    'multiple-tables-headerIDs',  ['headerIDs' => FALSE]];
+
         // Advanced
-        $array[] = ['spending-record-finished', 'spending-record-finished', $params];
-        $array[] = ['hidden-rows',              'hidden-rows',              $params];
-        $array[] = ['hidden-rows',              'hidden-rows-ignoreHidden', ['ignoreHidden' => TRUE]];
-        $array[] = ['timetable-caption',        'timetable-caption',        $params];
+        $array[] = ['spending-record-finished', 'spending-record-finished',     $params];
+        $array[] = ['hidden-rows',              'hidden-rows',                  $params];
+        $array[] = ['hidden-rows',              'hidden-rows-ignoreHidden',     ['ignoreHidden' => TRUE]];
+        $array[] = ['timetable-caption',        'timetable-caption',            $params];
+        $array[] = ['simple-table-error',       'simple-table-ignoreColumns1',  ['ignoreColumns' => ['IP', 'OS']]];
+        $array[] = ['simple-table-error',       'simple-table-ignoreColumns2',  ['ignoreColumns' => [3, 1]]];
+        $array[] = ['simple-table-error',       'simple-table-onlyColumns1',    ['onlyColumns' => ['IP', 'OS']]];
+        $array[] = ['simple-table-error',       'simple-table-onlyColumns2',    ['onlyColumns' => [3, 1]]];
 
         return $array;
     }
